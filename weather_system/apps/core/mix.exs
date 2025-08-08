0471a4439d3cdf6bce9074521375027de7e36b64
Lu <66143267+TolulopeVerissimo@users.noreply.github.com>
2025-08-08 16:03:05 -0400

    correction configuration

--- weather_system/apps/core/mix.exs
+++ weather_system/apps/core/mix.exs
@@ -19,16 +19,17 @@ defmodule Core.MixProject do
   def application do
     [
       extra_applications: [:logger],
-      mod: {Core.Application, []},
-      {:commanded, "~> 1.4"},
-    {:commanded_ecto_adapter, "~> 1.2"},
-    {:jason, "~> 1.2"}
+      mod: {Core.Application, []}
+
     ]
   end
 
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      {:commanded, "~> 1.4"},
+      {:commanded_eventstore_adapter, "~> 1.4"},
+      {:jason, "~> 1.4"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
       # {:sibling_app_in_umbrella, in_umbrella: true}
